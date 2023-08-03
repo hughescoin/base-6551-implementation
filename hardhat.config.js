316c2b7ceac6c2120dfbb7d706efd5eb45fe08c8
Patrick Hughes <patrick.hughes@coinbase.com>
2023-08-03 17:08:26 -0500

    add gasPrice field

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,20 +10,22 @@ module.exports = {
     // for mainnet
     'base-mainnet': {
       url: 'https://developer-access-mainnet.base.org',
-      accounts: [process.env.WALLET_KEY],
+      accounts: [process.env.WALLET_KEY, process.env.FRIEND_KEY],
       chainId: 84531,
     },
     // for testnet
     'base-goerli': {
       url: 'https://goerli.base.org',
-      accounts: [process.env.WALLET_KEY],
+      accounts: [process.env.WALLET_KEY, process.env.FRIEND_KEY],
       chainId: 84531,
+      gasPrice: 'auto',
     },
     // for local dev environment
     'base-local': {
       url: 'http://127.0.0.1:8545/',
-      accounts: [process.env.WALLET_KEY],
+      accounts: [process.env.WALLET_KEY, process.env.FRIEND_KEY],
       chainId: 31337,
+      gasPrice: 'auto',
     },
   },
   defaultNetwork: 'base-local',
