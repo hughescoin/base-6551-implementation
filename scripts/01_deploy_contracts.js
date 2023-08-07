11a79adedb4aa40a9fd81d78046da7ea26e608ff
Patrick Hughes <patrick.hughes@coinbase.com>
2023-08-07 17:35:11 -0500

    make scripts more modular

--- scripts/01_deploy_contracts.js
+++ scripts/01_deploy_contracts.js
@@ -1,8 +1,24 @@
+/**
+ * Copyright 2023 Coinbase Global, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 const hre = require('hardhat');
 const { deploymentFile } = require('../config');
 const storeDeploymentData = require('../storeDeploymentData');
 
-const wallet2 = process.env.FRIEND;
+const wallet2 = process.env.WALLET2_ADDR;
 async function deploy() {
   const provider = new hre.ethers.JsonRpcProvider('http://127.0.0.1:8545/');
   const signer = new hre.ethers.Wallet(process.env.WALLET_KEY, provider);
@@ -19,12 +35,6 @@ async function deploy() {
     ERC721DeploymentHash,
     deploymentFile
   );
-
-  console.log('Minting NFT...');
-  await ERC721Contract.mint(wallet2);
-  const tokenId = Number(await ERC721Contract.getTokenIds()) - 1;
-  const owner = await ERC721Contract.ownerOf(tokenId);
-  console.log(`TokenId ${tokenId} is owned by address:  ${owner}`);
 
   const Registry = await hre.ethers.deployContract('ERC6551Registry');
   await Registry.waitForDeployment();
