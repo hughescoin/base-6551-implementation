ea287e286920de1cd369dbe242d22b4338a7f171
Patrick Hughes <patrick.hughes@coinbase.com>
2023-08-07 17:35:38 -0500

    update env variable names

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,20 +10,20 @@ module.exports = {
     // for mainnet
     'base-mainnet': {
       url: 'https://developer-access-mainnet.base.org',
-      accounts: [process.env.WALLET_KEY, process.env.FRIEND_KEY],
+      accounts: [process.env.WALLET_KEY, process.env.WALLET2_KEY],
       chainId: 8453,
     },
     // for testnet
     'base-goerli': {
       url: 'https://goerli.base.org',
-      accounts: [process.env.WALLET_KEY, process.env.FRIEND_KEY],
+      accounts: [process.env.WALLET_KEY, process.env.WALLET2_KEY],
       chainId: 84531,
       gasPrice: 'auto',
     },
     // for local dev environment
     'base-local': {
       url: 'http://127.0.0.1:8545/',
-      accounts: [process.env.WALLET_KEY, process.env.FRIEND_KEY],
+      accounts: [process.env.WALLET_KEY, process.env.WALLET2_KEY],
       chainId: 31337,
       gasPrice: 'auto',
     },
